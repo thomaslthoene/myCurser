6e576b34ecfdf2abd2bf5ffd3fe3b3ef5d5e3bb9
Luke Thoene <luke.thoene@mindbodyonline.com>
2018-08-30 12:08:16 -0700

    updated timer and language settings

--- myRoom01.js
+++ myRoom01.js
@@ -1,7 +1,8 @@
+//Timer function.  Starts on page load.
 function checklength(i) {
     'use strict';
     if (i < 10) {
-        i = "0" + i;
+        i = i;
     }
     return i;
 }
@@ -25,7 +26,7 @@ function timer() {
     }
 }
 
-//the prisoner's inventory.  edit to equal "true" if it makes sense to your room.  There are opportunities to change these later based on player actions later in the function.
+//the prisoner's current circumstance.  Changed later based on player actions later in the function.
 let hasShovel = false;
 let hasKey01 = false;
 let hasKey02 = false;
@@ -35,88 +36,85 @@ let doorOpen=false;
 let candleLit=false;
 currentGender="female";
 pronounOne="she";
+pronounOneCapital="She"; //for use at sentence start
 pronounTwo="her";
-
-//current direction.  edit to equal player's direction on entering the room.  for instance, if they exit the previous room through a door in the north wall, they are still facing north in the new room.
+pronounTwoCapital="Her";
 currentDirection="North";
-let currentVictim=(Math.floor(Math.random() * 9) + 5)
 
-    if (currentVictim%2===0){
+//gender, set with a random number which then redefines pronouns for future strings.
+let currentPrisoner=(Math.floor(Math.random() * 100))
+
+    if (currentPrisoner%2===0){
         currentGender="male";
         pronounOne="he";
+        pronounOneCapital="He"; //for use at sentence start
         pronounTwo="him";
+        pronounTwoCapital="Him";
     }
 
+let currentPrisonerStatement=("You have activated the game.  Prisoner " + currentPrisoner + " is now in the room.  Try not to let this one die.");
+window.alert(currentPrisonerStatement);
 
-let currentVictimStatement=("You have activated the game.  Prisoner " + currentVictim + " is now in the room.  Try not to let " + pronounTwo + " die");
-window.alert(currentVictimStatement);
-
-//storyline progress responses.  these will change based on the story of your room.  For instance, if the clue for the end game is on the west wall and they enter "face west", the response string may equal "the numbers 1 and 2 are scrawled on the wall in brownish red liquid.  Could it be blood?"
-let response01="the door is now unlocked";
-let response02="The hinges wail in deathly squeals...the rust of ages breaking free.  A dark doorway stands before you";
-let response03="You don't have a key!";
+//storyline progress responses.  
+let response01="The door is now unlocked";
+let response02="The hinges squeal...the rust of ages breaking free.  A dark doorway stands before " + pronounTwo + ". " + pronounOneCapital + " seems unwilling to move forward.";
+let response03=pronounOneCapital + " doesn't have a key!";
 
 //gameplay responses.  They should follow the appropriate command entered.  For instance, if user enters "face north", add "return faceNorthResponse"
-let faceNorthResponse="You are now facing North.  Hundreds of scratch marks mar the plaster";
-let faceEastResponse="You are now facing East.  Something gleams in the dim light. You have found a key.";
-let faceSouthResponse="You are now facing South.  Someone has sprayed griffiti here.  It says, \"look away from dixieland\"";
-let faceWestResponse="A door of iron bars.  It looks as though it hasn't been opened in ages.  Even though the stairs beyond it lead downward, this appears to the only escape from your captivity";
-/*
-Available commands are:
-1 "face north"
-2 "face east"
-3 "face south"
-4 "face west"
-5 "pickup key"
-6 "use key"
-7 "use door"
-8 "use stairs"
-9 "inventory"
-10 "light candle"
-11 "pickup key"
-12 "use key"
-13 "pickup shovel"
-14 "use shovel"
-*/
-let mainImage="curserE.png";
+faceNorthResponse=pronounOneCapital + " is now facing North.  Hundreds of scratch marks mar the plaster";
+faceEastResponse=pronounOneCapital + " is now facing East.  Something gleams in the dim light. " + pronounOneCapital + " has found a key. ";
+faceSouthResponse=pronounOneCapital + " is now facing South.  Someone has sprayed griffiti here.  It says, \"look away from dixieland\"";
+faceWestResponse=pronounOneCapital + " is now facing west." + pronounOneCapital + " sees a locked door of iron bars.  Beyond it stairs...leading downward";
 
 function roomTemplate(){
 
     let moveData = document.getElementById("playerMove");
     let userInput = moveData.value;
 
-//these are the basic "face" (direction) commands and the results they should display.
+//move and look results in the dark.
 if (candleLit===false && userInput==="look around"){
-    
+    return window.alert(pronounOneCapital + " cannot see a thing.  It is pitch black.");
+
 }else if (candleLit===false && userInput==="face north"){
-    window.alert("You cannot see a thing.  It is pitch black.");
-    currentDirection="north";
+    return window.alert(pronounOneCapital + " cannot see a thing.  It is pitch black.");
+
+}else if (candleLit===false && userInput==="walk north"){
+    window.alert("No.  You've killed " + pronounTwo + ".  " + pronounOneCapital + " couldn't see our 'surprise.'  Not smart to move about in the dark.");
+    location.reload();
 
 }else if (candleLit===false && userInput==="face east"){
-    window.alert("You cannot see a thing.  It is pitch black.");
-    currentDirection="east";
+    return window.alert(pronounOneCapital + " cannot see a thing.  It is pitch black.");
+
+}else if (candleLit===false && userInput==="walk east"){
+    window.alert("No.  You've killed " + pronounTwo + ".  " + pronounOneCapital + " couldn't see our 'surprise.'  Not smart to move about in the dark.");
+    location.reload();
 
 }else if (candleLit===false && userInput==="face south"){
-    window.alert("You cannot see a thing.  It is pitch black.");
-    currentDirection="south";
+    return window.alert(pronounOneCapital + " cannot see a thing.  It is pitch black.");
+
+}else if (candleLit===false && userInput==="walk south"){
+    window.alert("No.  You've killed " + pronounTwo + ".  " + pronounOneCapital + " couldn't see our 'surprise.'  Not smart to move about in the dark.");
+    location.reload();
 
 }else if (candleLit===false && userInput==="face west"){
-    window.alert("You cannot see a thing.  It is pitch black.");
-    currentDirection="west";
+    return window.alert(pronounOneCapital + " cannot see a thing.  It is pitch black.");
 
-}else if (candleLit===true && userInput==="look around"){
-    window.alert("In the dim light you can make out four walls - brick.  The corners glisten with seeping water");
-    img01.src = "lookAround.jpg";
+}else if (candleLit===false && userInput==="walk west"){
+    window.alert("No.  You've killed " + pronounTwo + ".  " + pronounOneCapital + " couldn't see our 'surprise.'  Not smart to move about in the dark.");
+    location.reload();
+
+//post "inventory" (candle lit) commands
 
 }else if (userInput==="inventory"){
-    img01.src = "darkBrick.jpeg";
-    window.alert("you have a small candle and a lighter.  As you light the candle, the room takes shape around you.");
+    window.alert("Well done.  You have figured out the first command. Here..." +  pronounOneCapital + " now has a small candle and a lighter.  It won't last long, but probably longer than " + minutes + " minutes ;)");
     candleLit=true;
+
+}else if (candleLit===true && userInput==="look around"){
+    window.alert("In the dim light " + pronounOne + "can make out four walls - brick.  Uh oh," + pronounOne + " says the corners are seeping water ;) " + minutes + " remaining.");
 
 }else if (candleLit===true && userInput==="face north"){
     currentDirection="north";
     window.alert(faceNorthResponse);
-    img01.src = "scratchMarks.jpeg";
 
 }else if (candleLit===true && userInput==="face east"){
     currentDirection="east";
@@ -126,12 +124,10 @@ if (candleLit===false && userInput==="look around"){
 }else if (candleLit===true && userInput==="face south"){
     currentDirection="south";
     window.alert(faceSouthResponse);
-    img01.src = "base1.jpg";
 
 }else if (candleLit===true && userInput==="face west"){
     currentDirection="west";
     window.alert(faceWestResponse);
-    img01.src = "ironBar.jpg";
 
 }else if (currentDirection==="west" && hasKey01===true && userInput==="use key"){
     window.alert(response01);
@@ -154,7 +150,8 @@ if (candleLit===false && userInput==="look around"){
     window.alert("You step to the cavernous opening before you.  A breeze whistles from beyond the light of your candle.  If you knew where you were...WHO you were, you might think this was a grand adventure.  As it is, it feels like certain death.  You take the first halting step down...Welcome To The Curser.");
     window.location.href = "https://thomaslthoene.github.io/myCurser/myRoom02.html";
 
-}else return window.alert("You cannot do that");
+} else window.alert("You wish");
+   // location.reload();
 }
 
 
